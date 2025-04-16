904d1492b2847aed8146eaf9ec182964ecae7452
Ace10079 <minions10karan@gmail.com>
2025-04-16 17:48:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Navbar } from "./Navbar";
+import { GridBackgroundDemo } from "./GridBackgroundDemo";
+import { AppleCardsCarouselDemo } from "./AppleCardsCarouselDemo";
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      {/* Header */}
+      <header className="flex items-center justify-between px-6 py-4 bg-transparent absolute top-0 left-0 w-full z-50">
+
+        {/* Logo */}
+        <div className="flex items-center space-x-2">
+          <img
+            src="./IMG_3157.JPG"
+            alt="Logo"
+            className="h-20 w-20 object-cover "
+          />
+        </div>
+
+        {/* Navbar */}
+        <div className="hidden md:block">
+          <Navbar />
+        </div>
+
+        {/* Contact Us */}
+      
+        {/* Mobile Menu Button (Optional) */}
+        <div className="md:hidden">
+          {/* You can hook this to a dropdown or drawer later */}
+          <button className="text-black focus:outline-none">☰</button>
+        </div>
+      </header>
+
+      {/* Main Content */}
+      <main className="">
+        <GridBackgroundDemo />
+        <AppleCardsCarouselDemo/>
+      </main>
+    </div>
+  );
+}
+
+export default App;

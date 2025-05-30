c7acacafac22eb6870d3c127ab27e22ebbfdca46
Ace10079 <minions10karan@gmail.com>
2025-05-30 12:43:10 +0530

    hero animation

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,13 +1,6 @@
 function Hero() {
   return (
     <div className="bg-[url('/bg.JPG')] bg-cover bg-center min-h-screen relative flex items-center justify-center">
-      {/* Camera Image */}
-      <img
-        src="./camera.PNG"
-        alt="camera"
-        className="w-[150px] h-[120px] absolute left-1/2 top-[16.66%] transform -translate-x-1/2 animate-float z-10"
-      />
-
       {/* Mic Image */}
       <img
         src="./mic.PNG"
@@ -22,19 +15,34 @@ function Hero() {
         className="absolute right-5 bottom-0 w-[250px] h-[230px] animate-sway z-10"
       />
 
-      {/* Stylized Centered Text */}
-      <div className="absolute z-20 text-white text-center mt-30 px-4 max-w-[90%]">
-        <p className="text-[2.5rem] md:text-5xl font-bold text-blue-500 tracking-wide uppercase">turning</p>
-        
-        <p className="text-[4rem] md:text-[6rem] font-black text-black leading-none">‘thoda sa idea’</p>
+      {/* Text Content */}
+      <div className="absolute z-20 text-white text-center px-4 max-w-[90%]">
+        {/* Line 1 - turning + camera */}
+        <div className="flex justify-center items-center animate-fadeInUp">
+          <p className="text-[2.5rem] md:text-5xl font-bold text-blue-500 tracking-wide  text-center mr-2">
+            turning
+          </p>
+          <img
+            src="./camera.PNG"
+            alt="camera"
+            className="w-[150px] h-[120px] animate-float z-10"
+          />
+        </div>
 
-        <div className="relative inline-block my-2">
+        {/* Line 2 - 'thoda sa idea' */}
+        <p className="text-[4rem] md:text-[6rem] font-black text-black leading-none  animate-waveText">
+          ‘thoda sa idea’
+        </p>
+
+        {/* Line 3 - into */}
+        <div className="relative inline-block my-2 animate-waveText" >
           <span className="absolute -top-1 -left-2 rotate-[-5deg] bg-yellow-400 px-2 py-1 text-black text-sm font-bold z-0">
             into
           </span>
         </div>
 
-        <p className="text-[3rem] md:text-[5rem] font-extrabold text-blue-600 leading-none tracking-tight drop-shadow-xl">
+        {/* Line 4 - BADA BRAND MAGIC */}
+        <p className="text-[3rem] md:text-[5rem] font-extrabold text-blue-600 leading-none tracking-tight drop-shadow-xl animate-pulseText">
           BADA BRAND MAGIC
         </p>
       </div>
